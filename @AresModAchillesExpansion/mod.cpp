24e3c723dc2427e643b07751a823c466feb00186
Kex <rc_mapper@yahoo.com>
2016-11-13 06:34:45 +0100

    update readme

--- @AresModAchillesExpansion/mod.cpp
+++ @AresModAchillesExpansion/mod.cpp
@@ -1,7 +1,7 @@
-name = "Ares Mod - Achilles Expansion (0.0.4c)";
+name = "Ares Mod - Achilles Expansion (0.0.4d)";
 picture = "achilles_Insignia.paa";
 actionName = "Website";
 action = "https://forums.bistudio.com/topic/191113-ares-mod-achilles-expansion/?p=3031548";
-tooltip = "Ares Mod - Achilles Expansion (0.0.4c)";
+tooltip = "Ares Mod - Achilles Expansion (0.0.4d)";
 overview = "Ares augments the existing Zeus functionality, expanding the toolset and making it easy to create more compelling missions on the fly.<br />The Achilles Expansion (AE) includes Ares Mod with even more functionalites. Some functionalities only work with ACE mod, but those mods are not mandatory in order to run Ares (AE) properly.";
 author = "Ares by Anton Struyk, AE by Kex";
