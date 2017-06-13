f776d43a9e45689e260ea2504ef88765047f182a
Ralfs <CreepPork@users.noreply.github.com>
2017-06-13 16:09:44 +0300

    Removed Enyo icon and changed it to Achilles

--- @AresModAchillesExpansion/addons/modules_f_achilles/Zeus/cfgVehiclesModulesZeus.hpp
+++ @AresModAchillesExpansion/addons/modules_f_achilles/Zeus/cfgVehiclesModulesZeus.hpp
@@ -3,11 +3,6 @@ class Achilles_Zeus_Module_Base : Achilles_Module_Base
 	Category = "Curator";
 };
 
-class Enyo_Zeus_Module_Base : Enyo_Module_Base
-{
-	Category = "Curator";
-};
-
 class Achilles_Module_Zeus_SwitchUnit : Achilles_Zeus_Module_Base
 {
 	scopeCurator = 2;
@@ -17,7 +12,7 @@ class Achilles_Module_Zeus_SwitchUnit : Achilles_Zeus_Module_Base
 	portrait = "\achilles\data_f_achilles\icons\icon_unit.paa";
 };
 
-class Achilles_Module_Zeus_AssignZeus : Enyo_Zeus_Module_Base
+class Achilles_Module_Zeus_AssignZeus : Achilles_Zeus_Module_Base
 {
 	scopeCurator = 2;
 	displayName = "$STR_ASSIGN_ZEUS";
