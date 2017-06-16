7e185216026cf0edf856011c9572230b223d6956
Kex <rc_mapper@yahoo.com>
2017-06-16 21:01:46 +0200

    new dev branch

--- @AresModAchillesExpansion/mod.cpp
+++ @AresModAchillesExpansion/mod.cpp
@@ -1,14 +1,14 @@
-name = "Ares Mod - Achilles Expansion (0.0.8)";
+name = "Ares Mod - Achilles Expansion (0.1.0)";
 picture = "\achilles\data_f_achilles\pictures\Achilles_Icon_005.paa";
 author = "Ares by Anton Struyk, AE by Kex";
-description = "Ares Mod - Achilles Expansion (0.0.8)";
+description = "Ares Mod - Achilles Expansion (0.1.0)";
 logo = "\achilles\data_f_achilles\pictures\Achilles_Icon_005.paa";
 logoSmall = "\achilles\data_f_achilles\icons\icon_achilles_small.paa";
 logoOver = "\achilles\data_f_achilles\pictures\Achilles_Icon_005.paa";
-tooltip = "Ares Mod - Achilles Expansion (0.0.8)";
+tooltip = "Ares Mod - Achilles Expansion (0.1.0)";
 tooltipOwned = "Ares Mod - Achilles Expansion Owned";
 actionName = "Website";
 action = "https://forums.bistudio.com/topic/191113-ares-mod-achilles-expansion/?p=3031548";
 overview = "Ares augments the existing Zeus functionality, expanding the toolset and making it easy to create more compelling missions on the fly.<br />The Achilles Expansion (AE) includes Ares Mod with even more functionalites. Some functionalities only work with ACE mod, but those mods are not mandatory in order to run Ares (AE) properly.";
 overviewPicture = "\achilles\data_f_achilles\pictures\Achilles_Icon_005.paa";
-overviewText = "Ares Mod - Achilles Expansion (0.0.8)";
+overviewText = "Ares Mod - Achilles Expansion (0.1.0)";
