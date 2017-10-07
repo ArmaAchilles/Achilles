97ceef5856bbfecffc06c7412fdf12a5b523d9be
Kex <rc_mapper@yahoo.com>
2017-10-07 20:10:04 +0200

    fix fatal error

--- @AresModAchillesExpansion/addons/modules_f_achilles/cfgFactionClasses.hpp
+++ @AresModAchillesExpansion/addons/modules_f_achilles/cfgFactionClasses.hpp
@@ -3,7 +3,7 @@ class CfgFactionClasses
 {
 	class NO_CATEGORY;
 	
-	class ace_zeus_Medical: {displayName = "ACE Medical";};
+	class ace_zeus_Medical {displayName = "ACE Medical";};
 	class Achilles_fac_Buildings: NO_CATEGORY {displayName = "$STR_BUILDINGS";};
 	class Achilles_fac_Objects: NO_CATEGORY {displayName = "$STR_OBJECTS";};
 };
