9348608bcfed46df0deeddbd44a3f5ecb2de879f
oOKexOo <rc_mapper@yahoo.com>
2018-04-10 17:58:59 +0200

    Remove old arsenal functions and modules

--- @AresModAchillesExpansion/addons/modules_f_ares/Arsenal/cfgVehiclesModulesArsenal.hpp
+++
@@ -1,5 +0,0 @@
-class Ares_Arsenal_Module_Base : Ares_Module_base
-{
-	//subCategory = "$STR_AMAE_ARSENAL";
-	Category = "Achilles_fac_Arsenal";	
-};
--- @AresModAchillesExpansion/addons/modules_f_ares/cfgFunctions.hpp
+++ @AresModAchillesExpansion/addons/modules_f_ares/cfgFunctions.hpp
@@ -3,7 +3,6 @@ class CfgFunctions
 {
 	class Ares // This bit will be prefixed when actually calling the function (e.g. "Achilles_fnc_...." )
 	{
-		#include "Arsenal\cfgFunctionsArsenal.hpp"
 		#include "Behaviours\cfgFunctionsBehaviours.hpp"
 		#include "DevTools\cfgFunctionsDevTools.hpp"
 		#include "Equipment\cfgFunctionsEquipment.hpp"
