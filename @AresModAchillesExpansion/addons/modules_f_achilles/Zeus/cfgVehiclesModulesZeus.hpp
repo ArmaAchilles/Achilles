7a9eb13178d915dad00dd9e7d23b6cf965a48fd2
Ralfs <CreepPork@users.noreply.github.com>
2019-02-17 23:11:40 +0200

    Revert "Fix inheritance (#458)" (#459)
    This reverts commit 67605d9fb322ad3b0da5a25fe8d6bf52e2ea6f7a.

--- @AresModAchillesExpansion/addons/modules_f_achilles/Zeus/cfgVehiclesModulesZeus.hpp
+++ @AresModAchillesExpansion/addons/modules_f_achilles/Zeus/cfgVehiclesModulesZeus.hpp
@@ -20,7 +20,7 @@ class Achilles_Module_Zeus_AssignZeus : Achilles_Zeus_Module_Base
 	icon = "\achilles\data_f_achilles\icons\icon_unit.paa";
 	portrait = "\achilles\data_f_achilles\icons\icon_unit.paa";
 };
-class ModuleHint_F : ModuleBootcampStage_F
+class ModuleHint_F : Module_F
 {
 	scopeCurator = 1;
 	Category = "Curator";
