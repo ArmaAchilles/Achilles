410412d58668be264af11defcccbac44cd700db6
Ralfs <creepporkstudios@gmail.com>
2019-06-23 00:32:02 +0300

    Fixed profile GUI colors not working #477

--- addons/dialog/script_component.hpp
+++ addons/dialog/script_component.hpp
@@ -19,7 +19,6 @@
 
 #include "\a3\ui_f\hpp\defineDIKCodes.inc"
 #include "\a3\ui_f\hpp\defineCommonGrids.inc"
-#include "\a3\ui_f\hpp\defineCommonColors.inc"
 #include "\a3\ui_f_curator\ui\defineResinclDesign.inc"
 
 #define POS_X(N) ((N) * GUI_GRID_W + GUI_GRID_CENTER_X)
@@ -39,5 +38,11 @@
 #define POS_BACKGROUND_H(HEIGHT) (HEIGHT + 2 * CONTENT_SPACING)
 #define POS_TITLE_Y(HEIGHT)      (POS_BACKGROUND_Y(HEIGHT) - VERTICAL_SPACING - POS_H(1))
 #define POS_BUTTON_Y(HEIGHT)     (0.5 + HEIGHT / 2 + CONTENT_SPACING + VERTICAL_SPACING)
+
+#define THEME_COLOR_RGB_R "(profileNamespace getVariable ['GUI_BCG_RGB_R',0.13])"
+#define THEME_COLOR_RGB_G "(profileNamespace getVariable ['GUI_BCG_RGB_G',0.54])"
+#define THEME_COLOR_RGB_B "(profileNamespace getVariable ['GUI_BCG_RGB_B',0.21])"
+#define THEME_COLOR_ALPHA "(profileNamespace getVariable ['GUI_BCG_RGB_A',0.8])"
+#define THEME_COLORS {THEME_COLOR_RGB_R,THEME_COLOR_RGB_G,THEME_COLOR_RGB_B,THEME_COLOR_ALPHA}
 
 #include "script_idc.hpp"
