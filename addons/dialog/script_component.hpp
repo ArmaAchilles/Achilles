2c0d4aa784786cb71ed0c26f75ea3aa88a0bf227
Ralfs <creepporkstudios@gmail.com>
2019-06-24 19:42:55 +0300

    Disabled debug mode

--- addons/dialog/script_component.hpp
+++ addons/dialog/script_component.hpp
@@ -2,7 +2,7 @@
 #define COMPONENT_BEAUTIFIED Dialog
 #include "\z\achilles\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
+// #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define CBA_DEBUG_SYNCHRONOUS
 // #define ENABLE_PERFORMANCE_COUNTERS
