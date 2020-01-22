1ef1a96e0664dfe8b4e945b5f7d520a887c31639
oOKexOo <rc_mapper@yahoo.com>
2020-01-22 01:03:08 +0100

    Update version

--- @AresModAchillesExpansion/mod.cpp
+++ @AresModAchillesExpansion/mod.cpp
@@ -1,14 +1,14 @@
-name = "Achilles (1.2.2)";
+name = "Achilles (1.3.0)";
 picture = "\achilles\data_f_achilles\pictures\Achilles_Icon_005.paa";
 author = "ArmA 3 Achilles Mod Inc.";
-description = "Achilles (1.2.2)";
+description = "Achilles (1.3.0)";
 logo = "\achilles\data_f_achilles\pictures\Achilles_Icon_005.paa";
 logoSmall = "\achilles\data_f_achilles\icons\icon_achilles_small.paa";
 logoOver = "\achilles\data_f_achilles\pictures\Achilles_Icon_005.paa";
-tooltip = "Achilles (1.2.2)";
+tooltip = "Achilles (1.3.0)";
 tooltipOwned = "Achilles Owned";
 actionName = "Website";
 action = "https://github.com/ArmaAchilles/AresModAchillesExpansion";
 overview = "<t color='#d1111c'>Achilles</t> is a gameplay modification for Arma 3. It expands the Zeus real-time editor with many new additions as well as provides bug fixes.<br /><br /><t color='#d1111c'>Achilles</t> started as an expansion to Ares mod, which was created by Anton Struyk. Achilles became the de facto successor to Ares at the point the latter was no longer updated. Achilles has already grown into a splendid project, but new additions are still to come!";
 overviewPicture = "\achilles\data_f_achilles\pictures\Achilles_Icon_005.paa";
-overviewText = "Achilles (1.2.2)";
+overviewText = "Achilles (1.3.0)";
